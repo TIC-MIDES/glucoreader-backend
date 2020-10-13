4c227461f66f507177880e709673437820d7f7d8
Seba <sebastiancura97@gmail.com>
2020-10-13 09:21:25 -0300

    add value field

--- measures/admin.py
+++ measures/admin.py
@@ -3,7 +3,7 @@ from . import models
 
 class MeasureAdmin(admin.ModelAdmin):
     readonly_fields = ('creation_date',)
-    list_display = ('patient', 'creation_date')
+    list_display = ('patient', 'creation_date', 'value')
 
 
 admin.site.register(models.Measure, MeasureAdmin)
