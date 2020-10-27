aaa735deaff11c05083824714538def4eb7a0db1
Juan Pablo Zufriategui <juanpablo@biller.uy>
2020-10-26 22:42:17 -0300

    Se descartan valores absurdos

--- measures/api/views.py
+++ measures/api/views.py
@@ -60,19 +60,21 @@ class MeasureAPI(APIView):
                     items_list = list(ordered_values_dict.items())
                     if items_list[0][1] > items_list[1][1]: # Si hay un unico valor con numero de coincidencias max
                         measure.value = items_list[0][0]
-                        measure.save()
                     else:
                         return Response(http_response.format_response_failure('Error al reconocer los digitos'),
                                         status=status.HTTP_400_BAD_REQUEST)
                 elif len(values_dict) == 1:
                     measure.value = list(values_dict.keys())[0] # Se guarda el unico valor reconocido
-                    measure.save()
                 else:
                     return Response(http_response.format_response_failure('Error al reconocer los digitos'),
                                     status=status.HTTP_400_BAD_REQUEST)
             else: 
                 return Response(http_response.format_response_failure(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
-
+            if 50 < measure.value < 300:
+                measure.save()
+            else:
+                return Response(http_response.format_response_failure('Error al reconocer los digitos'),
+                                status=status.HTTP_400_BAD_REQUEST)
             response_data = MeasureSerializer(measure).data
             return Response(http_response.format_response_success(response_data), status=status.HTTP_200_OK)
         else:
