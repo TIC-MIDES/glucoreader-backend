6d1afbb5c8ed2c1c30aa94abae32a4b768d2f807
Seba <sebastiancura97@gmail.com>
2020-11-16 23:30:50 -0300

    Update url

--- utils/email.py
+++ utils/email.py
@@ -8,7 +8,7 @@ class Email:
     def send_email(measure):
         subject = "Alerta de valores de paciente en test de glucosa"
         destinators = [measure.patient.doctor.email]
-        params = {'first_name': measure.patient.first_name, 'last_name': measure.patient.last_name, 'ci':measure.patient.cedula, 'url': "https://glucoreader.herokuapp.com/medidas/"+measure.patient.cedula, 'measure': measure.value, 'creation_date': measure.creation_date}
+        params = {'first_name': measure.patient.first_name, 'last_name': measure.patient.last_name, 'ci':measure.patient.cedula, 'url': "https://glucotest.herokuapp.com/medidas/"+measure.patient.cedula, 'measure': measure.value, 'creation_date': measure.creation_date}
         html_content = render_to_string("measure.html", params)
         email = EmailMultiAlternatives(subject, from_email="Glucoreader <noreply@glucoreader.com>", to=destinators)
         email.attach_alternative(html_content, 'text/html')
