322c5a21e295e12b8c4163b4e70d8cee9f2f1c9a
Seba <sebastiancura97@gmail.com>
2020-11-27 01:12:10 -0300

    fix bug

--- measures/api/views.py
+++ measures/api/views.py
@@ -69,13 +69,13 @@ class MeasureAPI(APIView):
                     elif len(values_dict) == 1:
                         found = True
                         measure.value = list(values_dict.keys())[0] # Se guarda el unico valor reconocido
-                    else:
-                        cloudinary_response = cloudinary.uploader.upload("data:image/png;base64," + image_base64, public_id=img_name,
-                                                                            folder=f'Measures/{user.cedula}')
-                        measure.photo = cloudinary_response['url']
-                        measure.save()
-                        return Response(http_response.format_response_failure('Error al reconocer los digitos'),
-                                        status=status.HTTP_400_BAD_REQUEST)
+                    # else:
+                    #     cloudinary_response = cloudinary.uploader.upload("data:image/png;base64," + image_base64, public_id=img_name,
+                    #                                                         folder=f'Measures/{user.cedula}')
+                    #     measure.photo = cloudinary_response['url']
+                    #     measure.save()
+                    #     return Response(http_response.format_response_failure('Error al reconocer los digitos'),
+                    #                     status=status.HTTP_400_BAD_REQUEST)
                     if found:
                         if not 10 < measure.value < 500:
                             found = False
